2ee6287cb05fb5cbda2171ca69e77797e8d29d8d
holzalex <holzalex4schule@gmail.com>
2020-11-14 19:10:56 +0100

    Tutorial hinzugefügt, bugfixes

---
+++ Assets/To_Tutorial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class To_Tutorial : MonoBehaviour
+{
+    private int Tutorial;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Tutorial = SceneManager.GetActiveScene().buildIndex - 1;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(Tutorial);
+    }
+}
