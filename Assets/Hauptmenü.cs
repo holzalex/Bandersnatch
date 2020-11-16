65b48818cab457e18e24f33b9ff7497ee3a8171a
Tom Schrogl <tom-schrogl@web.de>
2020-11-16 18:19:57 +0100

    Ein Hauptmenü erstellt und teilweise funktion gegeben

---
+++ Assets/Hauptmenü.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hauptmenü : MonoBehaviour
+{
+    public void Spielstart() {
+        SceneManager.LoadScene("HQ");
+    }
+}
